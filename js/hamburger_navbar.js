32d05f75e4b413d8af5a1792e9ac015633f0015d
Sander Maurice Godard <sandermg@stud.ntnu.no>
2021-10-05 15:38:38 +0200

    Added lots of css so that we have a 'framework' to work off of

--- js/hamburger_navbar.js
+++ js/hamburger_navbar.js
@@ -5,7 +5,7 @@ menu.addEventListener("click", function () {
     if (dd.style.display == "none") {
         dd.style.display = "flex";
         menu.children[0].innerHTML = "close";
-        menu.style.backgroundColor = "#fefefe";
+        menu.style.backgroundColor = "var(--white)";
     } else {
         dd.style.display = "none";
         menu.children[0].innerHTML = "menu";
