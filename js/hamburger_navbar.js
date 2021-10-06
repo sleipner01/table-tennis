7f0608f587fc77fa14aeefee0c7fb75d494c9a62
Sander Maurice Godard <sandermg@stud.ntnu.no>
2021-10-06 19:45:45 +0200

    Upgraded hamburger_navbar.js to make the navbar very visible on load. Also added gallery background image for now.

--- js/hamburger_navbar.js
+++ js/hamburger_navbar.js
@@ -1,7 +1,10 @@
 bod = document.body.children;
 menu = document.getElementById("dd-menu");
 dd = document.getElementById("dropdown");
+na = document.getElementsByTagName("nav")[0]
 
+
+// Hides navbar dropdown
 function hide() {
     dd.style.display = "none";
     menu.children[0].innerHTML = "menu";
@@ -9,6 +12,7 @@ function hide() {
     return 1
 }
 
+// Toggles navbar dropdown
 function toggleDD() {
     if (dd.style.display == "none") {
         dd.style.display = "flex";
@@ -20,8 +24,9 @@ function toggleDD() {
     return 1
 }
 
+// Ensures you can click anything other than the navbar, to close the nav dropdown
 for (i=0; i<bod.length; i++) {
-    if (bod[i] == document.getElementsByTagName("nav")[0]) {
+    if (bod[i] == na) {
         // console.log("foudnNav")
         continue;
     }
@@ -34,3 +39,27 @@ for (i=0; i<bod.length; i++) {
 menu.addEventListener("click", e => {
     toggleDD();
 });
+
+// Makes the navbar and especially the menu very visible on page load.
+function menuBlink() {
+    na.style.transition = "ease 1s";
+    menu.style.transition = "ease 1s";
+    na.style.backgroundColor = "var(--white)";
+    menu.style.backgroundColor = "var(--white)";
+
+    setTimeout(function() {
+        //your code to be executed after 1 second
+        na.style.backgroundColor = "";
+    }, 2000);
+    setTimeout(function() {
+        //your code to be executed after 1 second
+        menu.style.backgroundColor = "";
+        na.style.transition = "";
+    }, 3000);
+    setTimeout(function() {
+        //your code to be executed after 1 second
+        menu.style.transition = "";
+    }, 4000);
+}
+
+menuBlink();
