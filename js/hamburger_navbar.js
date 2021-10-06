0b546f296b248d5f53ef0244d011e8b683f30601
Sander Maurice Godard <sandermg@stud.ntnu.no>
2021-10-06 18:55:53 +0200

    Fikset hamburger_navbar 100%, la til brief desc og deadline på mine deler av prosjektet.

--- js/hamburger_navbar.js
+++ js/hamburger_navbar.js
@@ -1,14 +1,36 @@
+bod = document.body.children;
 menu = document.getElementById("dd-menu");
 dd = document.getElementById("dropdown");
 
-menu.addEventListener("click", function () {
+function hide() {
+    dd.style.display = "none";
+    menu.children[0].innerHTML = "menu";
+    menu.style.backgroundColor = "";
+    return 1
+}
+
+function toggleDD() {
     if (dd.style.display == "none") {
         dd.style.display = "flex";
         menu.children[0].innerHTML = "close";
         menu.style.backgroundColor = "var(--white)";
     } else {
-        dd.style.display = "none";
-        menu.children[0].innerHTML = "menu";
-        menu.style.backgroundColor = "";
+        hide();
     }
+    return 1
+}
+
+for (i=0; i<bod.length; i++) {
+    if (bod[i] == document.getElementsByTagName("nav")[0]) {
+        // console.log("foudnNav")
+        continue;
+    }
+    bod[i].addEventListener("click", function () {
+        hide();
+    });
+    // console.log(bod[i])
+}
+
+menu.addEventListener("click", e => {
+    toggleDD();
 });
