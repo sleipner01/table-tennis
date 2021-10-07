af42a7889666af24a6ca8209ee7c29d81af06ddf
Ole Andreas <oarorvik@stud.ntnu.no>
2021-10-07 14:54:59 +0200

    +functions in training_table.js

--- js/training_table.js
+++ js/training_table.js
@@ -1,7 +1,32 @@
 const events = [
   {name: "Training", date: "1/10/2021", color: "green"},
+  {name: "Match", date: "3/10/2021", color: "orange"},
 ]
 
-const months = [
-  {name: "Januar", days: 31},
+const months = year => [
+  {name: "January", days: 31}, {name: "February", days: isLeapYear(year) ? 29 : 28},
+  {name: "March", days: 31}, {name: "April", days: 30},
+  {name: "May", days: 31}, {name: "June", days: 30},
+  {name: "July", days: 31}, {name: "August", days: 31},
+  {name: "September", days: 30}, {name: "October", days: 31},
+  {name: "November", days: 30}, {name: "December", days: 31},
 ]
+
+const isLeapYear = year => 
+  year % 400 === 0 ? true :
+  year % 100 === 0 ? false :
+  year % 4 === 0   ? true :
+                     false
+
+const range = (start, end) =>
+  start === end ? [start] :
+                  [start, ...range(start+1, end)]
+
+const dateTo = (date, format) => date
+
+const dateStrToArr = str => str.split("/",3)
+const dateArrToObj = arr => ({day: arr[0], month: arr[1], year: arr[2]})
+const dateStrToObj = str => dateArrToObj(dateStrToArr(str))
+
+let displayedMonth = "/11/2021"
+
