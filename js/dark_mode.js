dedcb2fa7756b8ec292c9639b2500030c1811157
Ole Andreas <oarorvik@stud.ntnu.no>
2021-10-07 23:53:48 +0200

    dark_mode.js restyling and refactoring

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,30 +1,23 @@
-const color = (propValue, lightModeValue, darkModeValue) => ({
-  prop:      propValue,
-  lightMode: lightModeValue,
-  darkMode:  darkModeValue
-})
-
 const colors = [ 
-//color(cssProperty,   lightMode, darkMode),
-  color("--red",       "#A8000F", "#A8000F"),
-  color("--lightgray", "#E5E5E5", "#E5E5E5"),
-  color("--darkgray",  "#828387", "#828387"),
-  color("--white",     "#FEFEFE", "#010101"),
-  color("--black",     "#010101", "#FEFEFE"),
-]
+  {property: "--red",       lightMode: "#A8000F", darkMode: "#A8000F"},
+  {property: "--lightgray", lightMode: "#E5E5E5", darkMode: "#E5E5E5"},
+  {property: "--darkgray",  lightMode: "#828387", darkMode: "#828387"},
+  {property: "--white",     lightMode: "#FEFEFE", darkMode: "#010101"},
+  {property: "--black",     lightMode: "#010101", darkMode: "#FEFEFE"}
+];
 
-const root = document.querySelector(":root")
+const root = document.querySelector(":root");
 
 const setColor = (color, mode) => root
   .style
-  .setProperty(color.prop, color[mode])
+  .setProperty(color.property, color[mode]);
 
 const setColors = mode => colors
-  .map(color => setColor(color, mode))
+  .map(color => setColor(color, mode));
 
-let isDarkMode = false
+let isDarkMode = false;
 
 const toggleDarkMode = () => {
-  isDarkMode = !isDarkMode
-  isDarkMode ? setColors("darkMode") : setColors("lightMode")
+  isDarkMode = !isDarkMode;
+  isDarkMode ? setColors("darkMode") : setColors("lightMode");
 }
