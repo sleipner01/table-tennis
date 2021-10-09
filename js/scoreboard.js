bb8f9b781351b6c337af1249eaa632bf0d3d5a21
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2021-10-09 20:13:11 +0200

    added random scores on scoreTable just for fun

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -6,8 +6,9 @@ function getData(dataTable) {
 
     // For each row of info, make an object in names
     for(let i = 0; i < scoreData.rows.length; i++) {
-        var data = scoreData.rows[i].cells;
-        names.push({name: data[0].innerText, score: data[1].innerText});
+        let data = scoreData.rows[i].cells;
+        // names.push({name: data[0].innerText, score: data[1].innerText});
+        names.push({name: data[0].innerText, score: Math.floor(Math.random() * 100)});
     }
     // Sort the names array
     names.sort(function (a, b) {
