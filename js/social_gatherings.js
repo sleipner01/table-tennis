23da21e9d75fb63d2f367e579cf5f261a996e140
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2021-10-11 17:55:25 +0200

    beginning of dynamic info - social-gatherings

--- js/social_gatherings.js
+++ js/social_gatherings.js
@@ -1,19 +1,32 @@
-// Year Control
-const previousYear = document.getElementById('previousYear');
-previousYear.addEventListener('click', yearChange);
-const nextYear = document.getElementById('nextYear');
-nextYear.addEventListener('click', yearChange);
-var currentYear = document.getElementById('currentYear');
-currentYear.innerText = new Date().getFullYear();
+// Year and information control
+const previousYearEl = document.getElementById('previousYear');
+    previousYearEl.addEventListener('click', yearChange);
+const nextYearEl = document.getElementById('nextYear');
+    nextYearEl.addEventListener('click', yearChange);
+var currentYearEl = document.getElementById('currentYear');
+    currentYearEl.innerText = new Date().getFullYear();
+var galleryEl = document.getElementById('social_gatherings_gallery');
+
+
+const dateObj = (day,month,year) => ({day: day, month: month, year: year});
+
+const social_events = [
+    {eventType: "gathering", date: dateObj(1,10,2021), name: "Field trip to Moskva", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ratione, quisquam ipsum officiis mollitia repellat at sequi, odit magnam, saepe recusandae praesentium repudiandae. Illo nihil et culpa totam consequuntur perferendis.", pictureURL: "../../bordtennis/media/gathering-images/magnus_domination.jpg"},
+    {eventType: "tournament", date: dateObj(1,10,2021), name: "Kiev", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ratione, quisquam ipsum officiis mollitia repellat at sequi, odit magnam, saepe recusandae praesentium repudiandae. Illo nihil et culpa totam consequuntur perferendis.", pictureURL: "../../bordtennis/media/gathering-images/equipment.jpg"},
+    {eventType: "gathering", date: dateObj(1,10,2021), name: "Pong n' pizza", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ratione, quisquam ipsum officiis mollitia repellat at sequi, odit magnam, saepe recusandae praesentium repudiandae. Illo nihil et culpa totam consequuntur perferendis.", pictureURL: "../../bordtennis/media/gathering-images/tabletennis_table.jpg"},
+  ];
+
 
 function yearChange(e) {
     if(e.target.id == 'nextYear') {
-        currentYear.innerText = Number(currentYear.innerText, 10) + 1;
+        currentYearEl.innerText = Number(currentYearEl.innerText, 10) + 1;
     }
     else if(e.target.id == 'previousYear') {
-        currentYear.innerText = Number(currentYear.innerText) - 1;
+        currentYearEl.innerText = Number(currentYearEl.innerText) - 1;
     }
 }
+
+
 
 
 
