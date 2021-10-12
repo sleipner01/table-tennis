2d66815ff8adacf800c0a1deda9a9adb875b0d05
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2021-10-12 03:13:37 +0200

    cropped some test phootos for homepage

--- js/front-page-gallery.js
+++ js/front-page-gallery.js
@@ -14,8 +14,8 @@ document.getElementsByTagName('body')[0].onscroll = function slowScroll() {
 
 
 // Change home page picture every other second
-const pictures = ['equipment.jpg', 'equipment2.jpg', 'man1.jpg', 'tabletennis_table.jpg'];
-const interval = 5; // Interval in seconds
+const pictures = ['equipment.jpg', 'equipment2.jpg', 'man1-cropped.jpg', 'tabletennis_table_cropped.jpg'];
+const interval = 10; // Interval in seconds
 var index = 0;
 galleryEl.style.backgroundImage = 'url(../bordtennis/media/home-images/'+ pictures[0] + ')';
 
